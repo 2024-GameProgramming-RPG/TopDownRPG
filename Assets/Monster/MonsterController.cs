3da0814d237070925203d9f6ec252ae97efc7df7
Choi JunHo <duekzim@gmail.com>
2024-11-20 19:36:47 +0900

    Feat: Monster animation add

--- Assets/Monster/MonsterController.cs
+++ Assets/Monster/MonsterController.cs
@@ -5,6 +5,7 @@ public class MonsterController : MonoBehaviour
 {
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator;    
     public float moveSpeed = 1f;
 
     void OnMove(InputValue movementValue) // 메서드 이름 수정
@@ -14,12 +15,27 @@ public class MonsterController : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        this.rb = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     void FixedUpdate() 
     {
         Vector2 movement = movementInput * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + movement);
+        if (movementInput != Vector2.zero)
+        {
+            float directionX = movementInput.x;
+            float directionY = movementInput.y;
+            this.animator.SetBool("isWalking", true);
+            this.animator.SetFloat("DirectionX", directionX);
+            this.animator.SetFloat("DirectionY", directionY);
+            Debug.Log($"DirectionX: {directionX}, DirectionY: {directionY}");
+
+        }
+        else
+        {
+            this.animator.SetBool("isWalking", false);
+        }
     }
 }
